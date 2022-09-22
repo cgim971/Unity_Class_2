0e9848f08e622d58669cec7befea666cff71b807
ChanheeKim <cgim971@gmail.com>
2022-09-22 20:18:53 +0900

    unity

--- Assets/AtkStateMachineBehaviour.cs
+++ Assets/AtkStateMachineBehaviour.cs
@@ -11,7 +11,7 @@ public class AtkStateMachineBehaviour : StateMachineBehaviour
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.GetComponent<stateAtkController>()EventStateAtkStart();
+        //animator.gameObject.GetComponent<stateAtkController>()EventStateAtkStart();
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
